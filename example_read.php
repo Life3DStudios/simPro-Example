009d7cbe8dd04711897bfbd76901a09f3551c44f
Life3DStudios <wes@life3dstudios.com>
2014-10-16 12:35:06 -0400

    Removed prod oauth cred

--- example_read.php
+++ example_read.php
@@ -7,16 +7,13 @@ require_once 'client.php';
 //$consumer = 'life3d-studios';
 //$consumer_secret = 'dUNZUsiTZg1enuO2il9kHeuar-IL1uQ5H66FC1qG0VeSu9s1MquSY5YrMxavq_KUnNUEqoi56e4ujcxC6WDslA';
 
-// Live system
-$host = 'mcse.simprocloud.com';
-$consumer = 'life3d-directaccess-c';
-$consumer_secret = 'P67zVIui4qMM6mS10bpZ_ArJ-sdx3DmhrqPJJrN9Td91qvyKEJVdz_QM1kWKvcFOkfQ8bMFiRpJDUjntevxe0A';
-
 $client = new SimPro\Api\Client($host, $consumer, $consumer_secret);
 
 
 // Sandbox company id
 // $company_id = 29
+
+// Live system
 $company_id = 0;
 $company = $client->CompanyRetrieve($company_id);
 
