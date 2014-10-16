0c00af26fceca58c10bd5019f7770a292587081e
Life3DStudios <wes@life3dstudios.com>
2014-10-16 12:33:08 -0400

    Cleaned up example_read.php
    Removed extra code and made comments

--- example_read.php
+++ example_read.php
@@ -2,33 +2,31 @@
 require_once 'library/autoload.php';
 require_once 'client.php';
 
-//
+// Our sandbox direct access credentials
 //$host = 'sandbox.simpro.co';
 //$consumer = 'life3d-studios';
 //$consumer_secret = 'dUNZUsiTZg1enuO2il9kHeuar-IL1uQ5H66FC1qG0VeSu9s1MquSY5YrMxavq_KUnNUEqoi56e4ujcxC6WDslA';
-//
 
-/*
- * $settings['oAuth']['host'] = 'https://mcse.simprocloud.com/api/';
-$settings['oAuth']['consumer'] = 'life3d-directaccess-c';
-$settings['oAuth']['consumer_secret'] ='P67zVIui4qMM6mS10bpZ_ArJ-sdx3DmhrqPJJrN9Td91qvyKEJVdz_QM1kWKvcFOkfQ8bMFiRpJDUjntevxe0A';
-
- */
-
+// Live system
 $host = 'mcse.simprocloud.com';
 $consumer = 'life3d-directaccess-c';
 $consumer_secret = 'P67zVIui4qMM6mS10bpZ_ArJ-sdx3DmhrqPJJrN9Td91qvyKEJVdz_QM1kWKvcFOkfQ8bMFiRpJDUjntevxe0A';
 
-
 $client = new SimPro\Api\Client($host, $consumer, $consumer_secret);
 
+
+// Sandbox company id
+// $company_id = 29
+$company_id = 0;
+$company = $client->CompanyRetrieve($company_id);
+
+
+// Outputing the client object for verification
 echo '<pre>';
 var_dump($client);
 echo '</pre>';
 
-$company = $client->CompanyRetrieve(0);
-
-
+// Viewing the company information
 echo '<pre>';
 var_dump($company);
 echo '</pre>';
