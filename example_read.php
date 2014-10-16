2abf0057b28c057775c114b963767c53bf2435ac
Life3DStudios <wes@life3dstudios.com>
2014-10-16 12:36:21 -0400

    Uncommented sandbox oauth credentials

--- example_read.php
+++ example_read.php
@@ -3,18 +3,18 @@ require_once 'library/autoload.php';
 require_once 'client.php';
 
 // Our sandbox direct access credentials
-//$host = 'sandbox.simpro.co';
-//$consumer = 'life3d-studios';
-//$consumer_secret = 'dUNZUsiTZg1enuO2il9kHeuar-IL1uQ5H66FC1qG0VeSu9s1MquSY5YrMxavq_KUnNUEqoi56e4ujcxC6WDslA';
+$host = 'sandbox.simpro.co';
+$consumer = 'life3d-studios';
+$consumer_secret = 'dUNZUsiTZg1enuO2il9kHeuar-IL1uQ5H66FC1qG0VeSu9s1MquSY5YrMxavq_KUnNUEqoi56e4ujcxC6WDslA';
 
 $client = new SimPro\Api\Client($host, $consumer, $consumer_secret);
 
 
 // Sandbox company id
-// $company_id = 29
+$company_id = 29;
 
 // Live system
-$company_id = 0;
+//$company_id = 0;
 $company = $client->CompanyRetrieve($company_id);
 
 
